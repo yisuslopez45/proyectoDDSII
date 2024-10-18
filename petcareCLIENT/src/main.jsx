19fd018f1a62d5f25cee80d9b32540e4c6603fe6
Carlos Daniel Corrales <carlox59681@gmail.com>
2024-10-18 12:20:09 -0500

    migracion de la parte del cliente a vite

---
+++ petcareCLIENT/src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import Dashboard from './pages/Dashboard/Dashboard'
+import NotFound from './pages/NotFound/NotFound'
+
+// route handler entry point
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />
+  },
+  {
+    path: '*',
+    element: <NotFound/>
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
